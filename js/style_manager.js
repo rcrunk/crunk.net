2f114c710886b1e7d7c9a92478bbbeade0c944db
Rolland Crunk <rc@crunk.biz>
2013-10-05 02:18:19 -0700

    Initial git introduction.

---
+++ js/style_manager.js
@@ -0,0 +1,115 @@
+function StyleManager() {
+  var MAX_STYLES = 8;
+  var CURRENT_STYLE_COOKIE_NAME = "currentStyle";
+  var FAVORITE_STYLE_COOKIE_NAME = "favoriteStyle";
+  var currentStyle = MAX_STYLES - 1;
+  var favoriteStyle = null;
+  var clicks = 0;
+  
+  this.initialize = function() {
+    var fromCookie = this.getCookie(FAVORITE_STYLE_COOKIE_NAME);
+    if (fromCookie != null) {
+      currentStyle = favoriteStyle = new Number(fromCookie);
+    } else {
+      fromCookie = this.getCookie(CURRENT_STYLE_COOKIE_NAME);
+      if (fromCookie != null) {
+        currentStyle = new Number(fromCookie);
+        
+      }
+    }
+    
+    // whack this in any case
+    this.deleteCookie(CURRENT_STYLE_COOKIE_NAME);
+    
+    this.loadStyle();
+  }
+
+  this.makeStyleSheetHref = function(styleIndex) {
+    return 'css/style-' + styleIndex  + '.css';
+  }
+
+  this.loadStyle = function() {
+    var filename = this.makeStyleSheetHref(currentStyle);
+    var fileref = document.createElement("link");
+    fileref.setAttribute("rel", "stylesheet");
+    fileref.setAttribute("type", "text/css");
+    fileref.setAttribute("href", filename);
+
+    document.getElementsByTagName("head")[0].appendChild(fileref)
+  }
+
+  this.clearStyle = function(styleIndex) {
+    var filename = this.makeStyleSheetHref(styleIndex);
+    var links = document.getElementsByTagName("link")
+    for (var i = links.length; i >=0 ; i--) { 
+      if (links[i]) {
+        var link = links[i];
+        var href = link.getAttribute("href");
+        if (href != null && href.indexOf(filename) != -1) {
+          link.parentNode.removeChild(link);
+        }
+      }
+    }
+  }
+
+  this.changeStyle = function() {
+    if (++clicks == MAX_STYLES) {
+      var toUrl = location.href.toString().replace(/\/$/, '/select_favorite_style.html');
+      this.setCookie(CURRENT_STYLE_COOKIE_NAME, ((currentStyle + 1) % MAX_STYLES), 5);
+      location.href = toUrl;
+      return true;
+    }
+
+    var previousStyle = currentStyle;
+    currentStyle = (currentStyle + 1) % MAX_STYLES;
+    this.loadStyle();
+    this.clearStyle(previousStyle);
+ 
+    return false;
+  }
+  
+  this.getFavoriteStyle = function() {
+    return this.getCookie(FAVORITE_STYLE_COOKIE_NAME);
+  }
+
+  this.setFavoriteStyle = function(favorite) {
+    favoriteStyle = favorite;
+    this.setCookie(FAVORITE_STYLE_COOKIE_NAME, favoriteStyle);
+  }
+  
+  this.clearFavoriteStyle = function() {
+    this.deleteCookie(FAVORITE_STYLE_COOKIE_NAME);
+  }
+  
+  this.setCookie = function(name, value, expirationMinutes) {
+    var expirationDate = null;
+    if (expirationMinutes) {
+      expirationDate = new Date();
+      expirationDate.setMinutes(expirationDate.getMinutes() + expirationMinutes);
+    }
+
+    var cookieValue = escape(value) 
+        + ((expirationDate == null) ? "" : "; expires=" + expirationDate.toUTCString());
+    document.cookie = name + "=" + cookieValue;
+  }
+
+  this.getCookie = function(name) {
+    var result = null;
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; ++i) {
+      var cookie = cookies[i];
+      var pivot = cookie.indexOf("=");
+      var cookieName = cookie.substr(0, pivot).replace(/^\s+|\s+$/g,"");
+      
+      if (cookieName == name) {
+        result = unescape(cookie.substr(pivot+1));
+      }
+    }
+
+    return result;
+  }
+  
+  this.deleteCookie = function(name) {
+    this.setCookie(name, 0, -45);
+  }
+}
